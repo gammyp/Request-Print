53ccc59a2eb072b0409ca0e0ee28014754e22daf
Pornlert Lorcharoenrath <gamebep@gmail.com>
2017-03-31 14:45:56 +0700

    Added Login Page / Remove AdminRegister Page

--- src/RequestPrintModel/Shop.java
+++ src/RequestPrintModel/Shop.java
@@ -6,4 +6,39 @@ public class Shop {
     private OwnerShop owner;
     
     
+    public Shop() {
+        
+    }
+    
+    public Shop(long shopId , Address address , OwnerShop owner) {
+        this.address = address;
+        this.shopId = shopId;
+        this.owner = owner;
+    }
+
+    public long getShopId() {
+        return shopId;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public OwnerShop getOwner() {
+        return owner;
+    }
+
+    public void setShopId(long shopId) {
+        this.shopId = shopId;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setOwner(OwnerShop owner) {
+        this.owner = owner;
+    }
+    
+    
 }
