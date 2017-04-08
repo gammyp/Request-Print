612485743073e948cbbea3b134815e212caa4a78
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2017-04-08 14:28:17 +0700

    Add Manage Book in shop and I con't edit exception in registerShop.

--- src/RequestPrintShop/ManageListBook.java
+++ src/RequestPrintShop/ManageListBook.java
@@ -27,35 +27,113 @@ public class ManageListBook extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         getContentPane().setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Sign out");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(600, 10, 50, 20);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Product ID", "Book name", "Price"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(190, 50, 452, 402);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 20)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Sign out");
+        jLabel1.setText("Manage Books");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 470, 160, 22);
+        jLabel1.setBounds(0, 40, 190, 30);
 
-        jPanel1.setBackground(new java.awt.Color(153, 153, 153));
-        jPanel1.setLayout(null);
+        jLabel2.setText("Name");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(10, 140, 40, 16);
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(20, 110, 160, 0);
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(20, 110, 160, 0);
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(20, 160, 150, 20);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("Home");
-        jPanel1.add(jLabel2);
-        jLabel2.setBounds(0, 110, 160, 30);
+        jLabel8.setText("Date");
+        getContentPane().add(jLabel8);
+        jLabel8.setBounds(10, 190, 50, 16);
 
-        getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 160, 500);
+        jLabel9.setText("Product ID");
+        getContentPane().add(jLabel9);
+        jLabel9.setBounds(10, 90, 60, 16);
+        getContentPane().add(jLabel10);
+        jLabel10.setBounds(20, 110, 150, 20);
+        getContentPane().add(jLabel11);
+        jLabel11.setBounds(20, 210, 150, 20);
 
-        setSize(new java.awt.Dimension(618, 547));
+        jLabel12.setText("Detail");
+        getContentPane().add(jLabel12);
+        jLabel12.setBounds(10, 240, 70, 16);
+        getContentPane().add(jLabel13);
+        jLabel13.setBounds(20, 260, 150, 80);
+
+        jLabel14.setText("Price of print");
+        getContentPane().add(jLabel14);
+        jLabel14.setBounds(10, 350, 80, 16);
+        getContentPane().add(jLabel15);
+        jLabel15.setBounds(20, 370, 150, 20);
+
+        jButton1.setText("Add");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(100, 420, 70, 30);
+
+        jButton2.setText("Back");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(20, 420, 70, 30);
+
+        setSize(new java.awt.Dimension(687, 524));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -95,8 +173,23 @@ public class ManageListBook extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
