3055e661c6ef4165070d41fedc6da02d3b000fce
Pornlert Lorcharoenrath <gamebep@gmail.com>
2017-05-19 17:58:12 +0700

    Backup

--- src/RequestPrintUser/Home.java
+++ src/RequestPrintUser/Home.java
@@ -291,7 +291,7 @@ public class Home extends javax.swing.JFrame {
         try {
             Connection con = ConnectionBuilder.getConnection();
             LoginEPrinting login = new LoginEPrinting();
-            PreparedStatement pstm = con.prepareStatement(" SELECT orderID, status FROM mydb.`Order` WHERE UserProfile_id = "+ login.getUserId());     
+            PreparedStatement pstm = con.prepareStatement(" SELECT orderID, status FROM Orders WHERE UserProfile_id = "+ login.getUserId());     
             ResultSet rs = pstm.executeQuery();
             while(rs.next()){
                 Vector v = new Vector();
--- src/RequestPrintUser/test.java
+++ src/RequestPrintUser/test.java
@@ -8,6 +8,7 @@ package RequestPrintUser;
 import RequestPrintDatabase.ConnectionBuilder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,15 +22,11 @@ public class test {
         Connection con;
         try {
             con = ConnectionBuilder.getConnection();
-            PreparedStatement pstm = con.prepareStatement("INSERT INTO mydb.Order VALUES (null,?,?,?,?,?,?,?)");
-            pstm.setInt(1, 1);
-            pstm.setString(2, "Pending Responding");
-            pstm.setString(3, "hsadkl");
-            pstm.setString(4, "hsadkl");
-            pstm.setString(5, "hsadkl");
-            pstm.setInt(6, 1);
-            pstm.setInt(7, 3);
-            pstm.executeUpdate();
+            PreparedStatement pstm = con.prepareStatement("SELECT orderID FROM mydb.Order ORDER BY orderID DESC LIMIT 0,1");
+            ResultSet rs = pstm.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getInt("orderID"));
+            }
             con.close();
         } catch (Exception ex) {
             System.out.println(ex);
